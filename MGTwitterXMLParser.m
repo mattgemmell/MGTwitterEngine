12afbd352f579355204762278d05a6ffe2027a03
Steve Streza <stevestreza@gmail.com>
2010-05-03 22:07:45 -0400

    Fix for #22

--- MGTwitterXMLParser.m
+++ MGTwitterXMLParser.m
@@ -113,6 +113,7 @@ connectionIdentifier:(NSString *)theIdentifier requestType:(MGTwitterRequestType
         // Change date-string into an NSDate.
 		NSLog(@"%@", [currentNode objectForKey:elementName]);
 		NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+		[dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US"]];
 		dateFormatter.dateFormat = @"EEE MMM dd HH:mm:ss +0000 yyyy";
 		NSDate *creationDate = [dateFormatter dateFromString:[currentNode objectForKey:elementName]];
 		[dateFormatter release];
