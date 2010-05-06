b4ca152915629c25bacc547d2fdf8ff540a6c14c
Will Harris <will.harris@netpriceonline.com>
2010-05-07 00:54:14 +0800

    Don't start body with a '?'.
    This fixes a bug where enableUpdatesFor: would fail with an erronous "Invalid / used nonce" error.

--- MGTwitterEngine.m
+++ MGTwitterEngine.m
@@ -459,7 +459,7 @@
 		}
         if (_clientSourceToken) {
             finalBody = [finalBody stringByAppendingString:[NSString stringWithFormat:@"%@source=%@", 
-                                                            (body) ? @"&" : @"?" , 
+                                                            (body) ? @"&" : @"" , 
                                                             _clientSourceToken]];
         }
         
